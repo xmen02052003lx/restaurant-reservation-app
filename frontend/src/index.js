b9b3c196a17212be3b310407cb41ef3cb64b7b68
xmen02052003lx <97832906+xmen02052003lx@users.noreply.github.com>
2024-06-19 15:43:15 +0700

    code giao diện trang đăng ký đăng nhập và cài đặt redux-toolkit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,16 +6,22 @@ import {
   Route,
   RouterProvider
 } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./store"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import HomeScreen from "./screens/HomeScreen"
+import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 )
@@ -23,7 +29,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
 
