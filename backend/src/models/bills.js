4d5ba6287d6251228b7bb287fad6af7515dc0742
duongnh <nguyenhaid98@yahoo,cin>
2024-07-27 22:49:01 +0700

    update bill

---
+++ backend/src/models/bills.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const billSchema = new mongoose.Schema({
+  tableId: { type: mongoose.Schema.Types.ObjectId, ref: 'tables', required: true },
+  items: [
+    {
+      itemId: { type: mongoose.Schema.Types.ObjectId, ref: 'menus', required: true },
+      quantity: { type: Number, required: true },
+      name: { type: String, required: true },
+      price: { type: Number, required: true },
+    }
+  ],
+  totalPrice: { type: Number, required: true },
+  paidAt: { type: Date, default: Date.now },
+});
+
+const Bill = mongoose.model('Bill', billSchema);
+
+module.exports = Bill;
---
+++ backend/src/routers/billRoute.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const {saveBill, getBill} = require('../components/bills')
+router.get('/bill/:tableId', getBill)
+router.post('/bill/pay/:tableId', saveBill);
+module.exports = router;
